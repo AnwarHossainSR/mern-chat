0e07df5d47475a7b634e607a8a7dac6a9ebaffdf
Restyled.io <commits@restyled.io>
2022-09-11 15:19:51 +0000

    Restyled by clang-format

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,7 @@
-import { combineReducers } from 'redux';
+import {combineReducers} from 'redux';
 
 import authReducer from './AuthReducer';
-//import chatReducer from "./ChatUserReducer";
+// import chatReducer from "./ChatUserReducer";
 
 export const reducers = combineReducers({
   authReducer,
