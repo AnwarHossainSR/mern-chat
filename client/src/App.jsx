facc4863660673052bb90773d880f50b42f8a139
Anwar Hossain <mahedianwar@gmail.com>
2022-09-11 10:48:20 +0600

    setup frontend and implemented authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Auth from './pages/Auth/Auth';
+import Chat from './pages/Chat/Chat';
 
-const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  const user = useSelector((state) => state.authReducer.authData);
+  return (
+    <div
+      className='App'
+      style={{
+        height: 'calc(100vh - 2rem)',
+      }}
+    >
+      <div className='blur' style={{ top: '-18%', right: '0' }}></div>
+      <div className='blur' style={{ top: '36%', left: '-8rem' }}></div>
+      <Routes>
+        <Route path='/' element={user ? <Navigate to='/chat' /> : <Auth />} />
+        <Route path='/chat' element={user ? <Chat /> : <Navigate to='/' />} />
+        <Route
+          path='*'
+          element={
+            <main style={{ padding: '1rem' }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+
+import authReducer from './AuthReducer';
+//import chatReducer from "./ChatUserReducer";
+
+export const reducers = combineReducers({
+  authReducer,
+});
